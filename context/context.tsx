687a9af8ae1b1b33647fea16e4ad5f486f7d03fa
Geng Sun <gsun@iastate.edu>
2024-11-02 00:40:39 -0500

    updated context

--- context/context.tsx
+++ context/context.tsx
@@ -1,10 +1,18 @@
 import { Dispatch, SetStateAction, createContext } from 'react';
 import { ChatMessage } from '@/types/chat-message';
+import { Tables } from '@/supabase/types';
+import { ChatSettings } from '@/types';
 
 interface ChatbotUIContext {
+  //PASSIVE CHAT STORE
+  chatSettings: ChatSettings | null;
+  setChatSettings: Dispatch<SetStateAction<ChatSettings>>;
+
   // ACTIVE CHAT STORE
   isGenerating: boolean;
   setIsGenerating: Dispatch<SetStateAction<boolean>>;
+  setFirstTokenReceived: Dispatch<SetStateAction<boolean>>;
+  setAbortController: Dispatch<SetStateAction<AbortController | null>>;
 
   // CHAT INPUT COMMAND STORE
   userInput: string;
@@ -15,11 +23,20 @@ interface ChatbotUIContext {
   setIsAssistantPickerOpen: Dispatch<SetStateAction<boolean>>;
   focusAssistant: boolean;
   setFocusAssistant: Dispatch<SetStateAction<boolean>>;
+
+  // ITEMS STORE
+  models: Tables<'models'>[];
 }
 export const ChatbotUIContext = createContext<ChatbotUIContext>({
+  //PASSIVE CHAT STORE
+  chatSettings: null,
+  setChatSettings: () => {},
+
   // ACTIVE CHAT STORE
   isGenerating: false,
   setIsGenerating: () => {},
+  setFirstTokenReceived: () => {},
+  setAbortController: () => {},
 
   // CHAT INPUT COMMAND STORE
   userInput: '',
@@ -30,4 +47,7 @@ export const ChatbotUIContext = createContext<ChatbotUIContext>({
   setFocusAssistant: () => {},
   isAssistantPickerOpen: false,
   setIsAssistantPickerOpen: () => {},
+
+  // ITEMS STORE
+  models: [],
 });
